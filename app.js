3d281e2ca5f7b1343e92777c189df9750fca8452
ALEXANDREBRUNETTI1A <165664490+ALEXANDREBRUNETTI1A@users.noreply.github.com>
2025-08-29 12:16:29 -0300

    s

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 function criaCartao(categoria, pergunta, resposta) {
-  let container = document.getElementById('container')
-  let cartao = document.createElement('article')
-  cartao.className = 'cartao'
-}
+let container = document.getElementById('container')
+let cartao = document.createElement('article')
+cartao.className = 'cartao'
 
 cartao.innerHTML = `
 <div class="cartao__conteudo">
@@ -18,13 +17,10 @@ cartao.innerHTML = `
 `
 let respostaEstaVisivel = false
 function viraCartao() {
-    respostaEstaVisivel = !respostaEstaVisivel
+respostaEstaVisivel = !respostaEstaVisivel
 cartao.classList.toggle('active', respostaEstaVisivel)
 }
-
-cartao.addEventListener('click', viraCartao) 
+cartao.addEventListener('click', viraCartao)
 
 container.appendChild(cartao)
-
-
-
+}
