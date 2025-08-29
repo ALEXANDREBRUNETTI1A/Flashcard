0c3ea7e80a36903cf756bb0efc27321f1b7a813e
ALEXANDREBRUNETTI1A <165664490+ALEXANDREBRUNETTI1A@users.noreply.github.com>
2025-08-29 12:23:43 -0300

    s

--- perguntas.js
+++ perguntas.js
@@ -1,35 +1,33 @@
 criaCartao(
-    'Programação',
-    'Oque é HTML?',
-    'O HTML (Linguagem de Marcação de HiperTexto)'
+'Programação',
+'O que é HTML?',
+'O HTML (Linguagem de Marcação de HiperTexto)'
 )
-
 criaCartao(
-    'Programação',
+'Programação',
 'O que é CSS?',
 'O CSS é uma linguagem de estilização.'
 )
-
-criaCartoa(
-    'Programação',
+criaCartao(
+'Programação',
 'O que é JavaScript?',
 'O JavaScript é uma linguagem de programação.'
 )
-
 criaCartao(
-    'Programação',
+'Programação',
 'O que é Front-end?',
 ' O Front-end compreende a parte visual de sites e aplicações.'
 )
-
 criaCartao(
-    'Programação',
+'Programação',
 'O que é Back-end?',
 'O Back-end compreende tudo aquilo que tem por trás de uma aplicação.'
 )
-
 criaCartao(
-    'Programação',
+'Programação',
 'O que é Full Stack?',
-'A Full Stack developer é uma pessoa que trabalha tanto com Front-end quanto com Backend.'
+
+'A Full Stack developer é uma pessoa que trabalha tanto com Front-end quanto com Back-
+end.'
+
 )
